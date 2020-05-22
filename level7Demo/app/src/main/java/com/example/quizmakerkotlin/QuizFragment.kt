dc7751a8e5a2378ef80f042587d1fb3ec75ee30b
yasmine By <benyasminenl@gmail.com>
2020-05-22 21:21:10 +0200

    Added all fragments

--- level7Demo/app/src/main/java/com/example/quizmakerkotlin/QuizFragment.kt
+++ level7Demo/app/src/main/java/com/example/quizmakerkotlin/QuizFragment.kt
@@ -9,14 +9,14 @@ import android.view.ViewGroup
 /**
  * A simple [Fragment] subclass.
  */
-class CreateQuizFragment : Fragment() {
+class QuizFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_quiz, container, false)
+        return inflater.inflate(R.layout.fragment_quiz, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
