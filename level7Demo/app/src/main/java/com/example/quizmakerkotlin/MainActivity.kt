9e58be4342ed35a36969e229f403e120045fbb74
yasmine By <benyasminenl@gmail.com>
2020-05-22 22:47:36 +0200

    navrgraph

--- level7Demo/app/src/main/java/com/example/quizmakerkotlin/MainActivity.kt
+++ level7Demo/app/src/main/java/com/example/quizmakerkotlin/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.quizmakerkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initNavigation()
     }
+    private fun initNavigation() {
+        // The NavController
+        val navController = findNavController(R.id.navHostFragment)
+
+        // Connect the navHostFragment with the ActionBar.
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
+
 }
